5c1c4a67d2393998a93e92e1d780b303c7665c81
chiragdoctor <chirag.doctor@gmail.com>
2020-07-03 19:10:54 +0100

    new build

---
+++ build/precache-manifest.75d315d56c0f105ddec7902e14590677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f63a8a2a992c5960892fcacf497e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0677063ab6fd32ca3e1",
+    "url": "/static/css/main.ef17679a.chunk.css"
+  },
+  {
+    "revision": "f847c94a75cc06555a81",
+    "url": "/static/js/2.d0a2e629.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0a2e629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0677063ab6fd32ca3e1",
+    "url": "/static/js/main.9f6ed38b.chunk.js"
+  },
+  {
+    "revision": "2f6440121027ae412502",
+    "url": "/static/js/runtime-main.5ea8c4ef.js"
+  },
+  {
+    "revision": "f6d9fc2799a3ed4da74feb2b75ab5ae6",
+    "url": "/static/media/spinner.f6d9fc27.gif"
+  }
+]);
